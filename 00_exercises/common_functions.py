722ce68a854ac6e9cad2d026608e040b712a6981
Matzcoder <chiaweiwoo123@gmail.com>
2018-01-21 19:49:28 +0800

    Added make_terrain_data() and plot_2d_scatter() into common functions

--- 00_exercises/common_functions.py
+++ 00_exercises/common_functions.py
@@ -1,5 +1,7 @@
 import numpy as np
+import random
 import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
 
 def plot_model_with_boundaries(model, X, y, title):
 	plt.scatter(X[np.argwhere(y==0).flatten(),0],X[np.argwhere(y==0).flatten(),1],s = 50, color = 'blue', edgecolor = 'k')
@@ -32,4 +34,26 @@ def plot_model_with_boundaries(model, X, y, title):
 	if len(np.unique(z)) > 1:
 	    plt.contour(s,t,z,colors = 'k', linewidths = 2)
 	plt.title(title)
+	plt.show()
+
+def make_terrain_data(n_points=1000):
+    random.seed(42)
+    grade = [random.random() for ii in range(0,n_points)]
+    bumpy = [random.random() for ii in range(0,n_points)]
+    error = [random.random() for ii in range(0,n_points)]
+    
+    features = [[gg, ss] for gg, ss in zip(grade, bumpy)]
+    targets = [round(grade[ii]*bumpy[ii]+0.3+0.1*error[ii]) for ii in range(0,n_points)]
+    
+    for ii in range(0, len(targets)):
+        if grade[ii]>0.8 or bumpy[ii]>0.8:
+            targets[ii] = 1.0
+
+    features = np.array(features)
+    targets = np.array(targets)
+
+    return features, targets
+
+def plot_2d_scatter(X, y, colors=['blue', 'red']):
+	plt.scatter(X[:,0], X[:,1], c=y, cmap=ListedColormap(colors)) 
 	plt.show()
